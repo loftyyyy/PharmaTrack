2fc3cd25b7b75e283848312c5e16030aaa81242a
loftyyyy <lofty08.business@gmail.com>
2025-07-05 21:23:39 +0800

    Finished implementing integration tests to the role controller

--- src/test/java/com/rho/ims/controller/RoleControllerIntegrationTest.java
+++ src/test/java/com/rho/ims/controller/RoleControllerIntegrationTest.java
@@ -2,8 +2,10 @@ package com.rho.ims.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rho.ims.config.SecurityConfig;
+import com.rho.ims.dto.UpdateRoleDTO;
 import com.rho.ims.model.Role;
 import com.rho.ims.respository.RoleRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -64,11 +66,6 @@ class RoleControllerIntegrationTest {
                     .andExpect(jsonPath("$.name").value("Name cannot be blank!"))
                     .andDo(print());
 
-
-
-
-
-
         }
 
 
@@ -76,20 +73,132 @@ class RoleControllerIntegrationTest {
 
     }
 
+    @Nested
+    class DeleteRoleTest {
+        long testRoleId;
 
-    @Test
-    void createRole() {
+        @BeforeEach
+        void setup(){
+
+            Role testRole = new Role();
+            testRole.setName("Test Role");
+            roleRepository.save(testRole);
+
+            testRoleId = testRole.getId();
+
+
+
+        }
+
+        @DisplayName("Should delete role")
+        @Test
+        void shouldReturnSuccessfulRequest_deleteRole() throws Exception {
+
+            mockMvc.perform(delete("/api/v1/roles/" + testRoleId).contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andExpect(content().string("Role deleted successfully"))
+                    .andDo(print());
+
+        }
+
+        @DisplayName("Should fail when passing invalid id that doesn't exist")
+        @Test
+        void shouldReturnBadRequest_invalidRole() throws Exception {
+            long invalidId = 99;
+
+            mockMvc.perform(delete("/api/v1/roles/" + invalidId).contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string("Error deleting role: Role not found with id: " + invalidId))
+                    .andDo(print());
+
+        }
+
     }
 
-    @Test
-    void deleteRole() {
+    @Nested
+    class GetRoleTest {
+        Role testRole;
+
+
+        @BeforeEach
+        void setup() {
+            Role testRole = new Role();
+            testRole.setName("Test Role");
+
+            roleRepository.save(testRole);
+            this.testRole = testRole;
+
+        }
+
+        @DisplayName("Should return role")
+        @Test
+        void shouldReturnSuccessfulRequest_retrieveRole() throws Exception {
+
+            mockMvc.perform(get("/api/v1/roles/" + testRole.getId()).contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andExpect(jsonPath("$.id").value(testRole.getId()))
+                    .andExpect(jsonPath("$.name").value(testRole.getName()))
+                    .andDo(print());
+
+        }
+
+        @DisplayName("Should fail when id is invalid")
+        @Test
+        void shouldReturnBadRequest_invalidRole() throws Exception {
+            long invalidId = 999;
+
+            mockMvc.perform(get("/api/v1/roles/" + invalidId).contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string("Error retrieving role: Role not found with id: " + invalidId) )
+                    .andDo(print());
+
+        }
+
+
+
     }
 
-    @Test
-    void getRole() {
-    }
 
-    @Test
-    void updateRole() {
+    @Nested
+    class UpdateRoleTest {
+        long testRoleId;
+
+        @BeforeEach
+        void setup(){
+            Role testRole = new Role();
+            testRole.setName("Test Role");
+            roleRepository.save(testRole);
+
+            testRoleId = testRole.getId();
+
+        }
+
+
+
+        @DisplayName("Should update role")
+        @Test
+        void shouldReturnSuccessfulRequest_updateRole() throws Exception {
+            UpdateRoleDTO updateRoleDTO = new UpdateRoleDTO();
+            updateRoleDTO.setName("New Test Role");
+
+            mockMvc.perform(put("/api/v1/roles/" + testRoleId).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(updateRoleDTO)))
+                    .andExpect(status().is2xxSuccessful())
+                    .andExpect(content().string("Role updated successfully"))
+                    .andDo(print());
+        }
+
+        @DisplayName("Should fail when invalid role id")
+        @Test
+        void shouldReturnBadRequest_updateRoleFail() throws Exception {
+            long invalidId = 999;
+            UpdateRoleDTO updateRoleDTO = new UpdateRoleDTO();
+            updateRoleDTO.setName("New Role Name");
+
+            mockMvc.perform(put("/api/v1/roles/" + invalidId).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(updateRoleDTO)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string("Error updating role: Role not found with id: " + invalidId))
+                    .andDo(print());
+        }
+
     }
 }
