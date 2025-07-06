5e5968178147ccf0ccb33494401bd4f35b52bcec
loftyyyy <lofty08.business@gmail.com>
2025-07-06 21:12:12 +0800

    Still thinking about what I should put in the product controller

---
+++ src/main/java/com/rho/ims/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.rho.ims.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+
+
+
+}
