fc3c473e91197ecae5b91535085e8edd0868ea90
loftyyyy <lofty08.business@gmail.com>
2025-10-09 00:16:08 +0800

    Implementing the reports, doing the sales report

---
+++ src/main/java/com/rho/ims/service/SalesReportService.java
@@ -0,0 +1,20 @@
+package com.rho.ims.service;
+
+import com.rho.ims.respository.SaleItemRepository;
+import com.rho.ims.respository.SaleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalesReportService {
+    private final SaleRepository saleRepository;
+    private final SaleItemRepository saleItemRepository;
+
+    public SalesReportService(SaleRepository saleRepository, SaleItemRepository saleItemRepository){
+        this.saleRepository = saleRepository;
+        this.saleItemRepository = saleItemRepository;
+    }
+
+
+
+}
+
