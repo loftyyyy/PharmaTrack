2ff662aefa38e279774cd087c100c0c547dd0fe3
loftyyyy <lofty08.business@gmail.com>
2025-10-13 11:26:17 +0800

    Changed the alert service to use product batch repository

--- src/main/java/com/rho/ims/service/LowStockAlertService.java
+++ src/main/java/com/rho/ims/service/LowStockAlertService.java
@@ -1,13 +1,18 @@
 package com.rho.ims.service;
 
 import com.rho.ims.respository.LowStockAlertRepository;
+import com.rho.ims.respository.ProductBatchRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LowStockAlertService {
     private final LowStockAlertRepository lowStockAlertRepository;
+    private final ProductBatchRepository productBatchRepository;
 
-    public LowStockAlertService(LowStockAlertRepository lowStockAlertRepository){
+    public LowStockAlertService(LowStockAlertRepository lowStockAlertRepository, ProductBatchRepository productBatchRepository){
         this.lowStockAlertRepository = lowStockAlertRepository;
+        this.productBatchRepository = productBatchRepository;
     }
+
+    
 }
