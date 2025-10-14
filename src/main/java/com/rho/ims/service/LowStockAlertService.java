48042999118261a1089b4c723e6c6ea9d92fc7bf
loftyyyy <lofty08.business@gmail.com>
2025-10-14 23:39:56 +0800

    Implementing the low stock alert service

--- src/main/java/com/rho/ims/service/LowStockAlertService.java
+++ src/main/java/com/rho/ims/service/LowStockAlertService.java
@@ -1,5 +1,6 @@
 package com.rho.ims.service;
 
+import com.rho.ims.model.LowStockAlert;
 import com.rho.ims.respository.LowStockAlertRepository;
 import com.rho.ims.respository.ProductBatchRepository;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,10 @@ public class LowStockAlertService {
     public LowStockAlertService(LowStockAlertRepository lowStockAlertRepository, ProductBatchRepository productBatchRepository){
         this.lowStockAlertRepository = lowStockAlertRepository;
         this.productBatchRepository = productBatchRepository;
+    }
+
+    public LowStockAlert lowStockProducts(){
+        return null;
     }
 
     
