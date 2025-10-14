ce73029f1fa99b61879de605b0f1b053c602e2d1
loftyyyy <lofty08.business@gmail.com>
2025-10-15 00:09:08 +0800

    WIP: Low stock alert service

--- src/main/java/com/rho/ims/respository/LowStockAlertRepository.java
+++ src/main/java/com/rho/ims/respository/LowStockAlertRepository.java
@@ -1,7 +1,12 @@
 package com.rho.ims.respository;
 
+import com.rho.ims.model.LowStockAlert;
+import com.rho.ims.model.ProductBatch;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LowStockAlertRepository {
+public interface LowStockAlertRepository extends JpaRepository<LowStockAlert, Long> {
+
+    boolean existsByProductBatchAndResolvedFalse(ProductBatch productBatch);
 }
--- src/main/java/com/rho/ims/service/LowStockAlertService.java
+++ src/main/java/com/rho/ims/service/LowStockAlertService.java
@@ -4,7 +4,10 @@ import com.rho.ims.model.LowStockAlert;
 import com.rho.ims.model.ProductBatch;
 import com.rho.ims.respository.LowStockAlertRepository;
 import com.rho.ims.respository.ProductBatchRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LowStockAlertService {
@@ -16,8 +19,14 @@ public class LowStockAlertService {
         this.productBatchRepository = productBatchRepository;
     }
 
-    public LowStockAlert lowStockProducts(){
-        ProductBatch productBatch = productBatchRepository.findBy?
+    @Transactional
+    public LowStockAlert checkAndCreateLowStockAlerts(){
+        List<ProductBatch> productBatch = productBatchRepository.findByQuantityLessThan(10);
+
+        for(ProductBatch pb : productBatch){
+
+
+        }
         return null;
     }
 
