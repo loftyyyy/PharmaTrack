24c74a060bb7f349d9c16796e0b9087e49086f08
loftyyyy <lofty08.business@gmail.com>
2025-10-15 18:44:45 +0800

    Added a query for the low stock alert repository

--- src/main/java/com/rho/ims/respository/LowStockAlertRepository.java
+++ src/main/java/com/rho/ims/respository/LowStockAlertRepository.java
@@ -3,8 +3,10 @@ package com.rho.ims.respository;
 import com.rho.ims.model.LowStockAlert;
 import com.rho.ims.model.ProductBatch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,7 @@ public interface LowStockAlertRepository extends JpaRepository<LowStockAlert, Lo
     boolean existsByProductBatch(ProductBatch productBatch);
 
     Optional<LowStockAlert> findByProductBatch(ProductBatch productBatch);
+
+    @Query("SELECT pb FROM ProductBatch pb WHERE pb.quantity <= pb.product.minimumStock")
+    List<ProductBatch> findAllBelowMinimumStock();
 }
