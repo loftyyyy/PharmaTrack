b884c5273d49b9d112699179783f682242df5ffc
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-26 23:41:50 +0800

    Fixing syntax based on java standards

--- src/main/java/com/rho/ims/controller/PurchaseController.java
+++ src/main/java/com/rho/ims/controller/PurchaseController.java
@@ -23,12 +23,14 @@ public class PurchaseController {
     @PostMapping("/create")
     public ResponseEntity<?> createPurchase(@Valid @RequestBody PurchaseCreateDTO purchaseCreateDTO){
         Purchase purchase = purchaseService.savePurchase(purchaseCreateDTO);
+
         return ResponseEntity.ok().body(new PurchaseResponseDTO(purchase));
     }
 
     @GetMapping
     public ResponseEntity<?> getAllPurchase(){
         List<PurchaseResponseDTO> purchases = purchaseService.getAll().stream().map(purchase -> new PurchaseResponseDTO(purchase)).toList();
+
         return ResponseEntity.ok().body(purchases);
     }
 
@@ -40,18 +42,21 @@ public class PurchaseController {
     @PutMapping("/{id}")
     public ResponseEntity<?> updatePurchase(@Valid @RequestBody PurchaseUpdateDTO purchaseUpdateDTO, @PathVariable Long id){
         Purchase purchase = purchaseService.updatePurchase(purchaseUpdateDTO, id);
+
         return ResponseEntity.ok().body(new PurchaseResponseDTO(purchase));
     }
 
     @PutMapping("/{id}/confirm")
     public ResponseEntity<?> confirmPurchase(@PathVariable Long id){
         Purchase purchase = purchaseService.confirmPurchase((id));
+
         return ResponseEntity.ok().body(new PurchaseResponseDTO(purchase));
     }
 
     @PutMapping("/{id}/cancel")
     public ResponseEntity<?> cancelPurchase(@PathVariable Long id){
         Purchase purchase = purchaseService.cancelPurchase((id));
+
         return ResponseEntity.ok().body(new PurchaseResponseDTO(purchase));
     }
 
