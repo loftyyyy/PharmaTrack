79a64a8bc62941e8bbf001d8421f3dd3a70fa97f
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-26 23:40:06 +0800

    Fixing syntax based on java standards

--- src/main/java/com/rho/ims/controller/UserController.java
+++ src/main/java/com/rho/ims/controller/UserController.java
@@ -30,30 +30,35 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<?> createUser(@Valid @RequestBody RegisterRequest registerRequest) {
             userService.saveUser(registerRequest);
+
             return ResponseEntity.ok("User created successfully");
     }
 
     @GetMapping("/me")
     public ResponseEntity<UserResponseDTO> getCurrentUser(Authentication authentication) {
         User user = userService.findByName(authentication.getName());
+
         return ResponseEntity.ok(new UserResponseDTO(user));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getUser(@PathVariable long id) {
         User user = userService.findById(id);
+
         return ResponseEntity.ok(new UserResponseDTO(user));
     }
 
     @GetMapping()
     public ResponseEntity<?> getAllUser(){
         List<UserResponseDTO> users = userService.getAll();
+
         return ResponseEntity.ok().body(users);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@PathVariable long id, @Valid @RequestBody UserUpdateDTO userUpdateDTO) {
         User updatedUser = userService.updateUser(id, userUpdateDTO);
+
         return ResponseEntity.ok().body("Successfully updated profile");
     }
 
