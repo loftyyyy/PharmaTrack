6c0592535f4b74556797994f8337e710ad7c6dcf
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-26 23:40:49 +0800

    Fixing syntax based on java standards

--- src/main/java/com/rho/ims/controller/SaleController.java
+++ src/main/java/com/rho/ims/controller/SaleController.java
@@ -23,18 +23,21 @@ public class SaleController {
     @PostMapping("/create")
     public ResponseEntity<?> createSale(@Valid @RequestBody SaleCreateDTO saleCreateDTO) {
         Sale sale = saleService.saveSale(saleCreateDTO);
+
         return ResponseEntity.ok().body(new SaleResponseDTO(sale));
     }
 
     @GetMapping
     public ResponseEntity<?> getAll() {
         List<SaleResponseDTO> sales = saleService.getAll().stream().map(sale -> new SaleResponseDTO(sale)).toList();
+
         return ResponseEntity.ok().body(sales);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getSale(@PathVariable Long id) {
         Sale sale = saleService.getSale(id);
+
         return ResponseEntity.ok().body(new SaleResponseDTO(sale));
 
     }
@@ -42,21 +45,21 @@ public class SaleController {
     @PostMapping("/{id}/confirm")
     public ResponseEntity<?> confirmSale(@PathVariable Long id) {
         Sale sale = saleService.confirmSale(id);
+
         return ResponseEntity.ok().body(new SaleResponseDTO(sale));
     }
 
     @PostMapping("/{id}/void")
     public ResponseEntity<?> voidSale(@PathVariable Long id, @Valid @RequestBody SaleVoidDTO saleVoidDTO) {
         Sale sale = saleService.voidSale(id, saleVoidDTO);
+
         return ResponseEntity.ok().body(new SaleResponseDTO(sale));
     }
 
     @PostMapping("/{id}/cancel")
     public ResponseEntity<?> cancelSale(@PathVariable Long id) {
         Sale sale = saleService.cancelSale(id);
+
         return ResponseEntity.ok().body(new SaleResponseDTO(sale));
     }
-
-
-
 }
