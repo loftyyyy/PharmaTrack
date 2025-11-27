cd4ffa406a34b5aa386cd1ae46a7c36447ba2a76
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-28 00:47:44 +0800

    Fixing the syntax

--- src/main/java/com/rho/ims/controller/CategoryController.java
+++ src/main/java/com/rho/ims/controller/CategoryController.java
@@ -24,29 +24,33 @@ public class CategoryController {
         this.categoryService = categoryService;
         this.categoryRepository = categoryRepository;
     }
-    // CRUD
+
     @GetMapping
     public ResponseEntity<?> getAllCategories(){
         List<CategoryResponseDTO> categories = categoryService.getAll()
                 .stream().map(category -> new CategoryResponseDTO(category)).toList();
+
         return ResponseEntity.ok().body(categories);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getCategory(@PathVariable long id){
         Category category = categoryService.getCategory(id);
+
         return ResponseEntity.ok().body(new CategoryResponseDTO(category));
     }
 
     @PostMapping("/create")
     public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryCreateDTO categoryCreateDTO){
         Category category = categoryService.saveCategory(categoryCreateDTO);
+
         return ResponseEntity.ok().body(new CategoryResponseDTO(category));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateCategory(@PathVariable long id, @Valid @RequestBody CategoryUpdateDTO categoryUpdateDTO){
         Category category = categoryService.updateCategory(id, categoryUpdateDTO);
+
         return ResponseEntity.ok().body(new CategoryResponseDTO(category));
     }
 
